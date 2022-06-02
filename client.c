6951ec8f12194f7e2f7ee60fd7e9c67f93c1bf17
DavideGioia <giastru@DESKTOP-H4FTGGT.localdomain>
2022-06-02 22:52:38 +0200

    send and recive string from argumentgit status!

--- client.c
+++ client.c
@@ -13,12 +13,35 @@
 #include <signal.h>
 #include <unistd.h>
 #include "libft/includes/libft.h"
+#include "libft/includes/ft_printf.h"
+
+static void send_str(int pid, char *str)
+{
+    int i;
+    char single_char;
+
+    while (*str)
+    {
+        i = 8;
+        single_char = *str++;
+        while (i--)
+        {
+            if (single_char >> i & 1)
+                kill(pid, SIGUSR1);
+            else
+                kill(pid, SIGUSR2);
+            usleep(100);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+
+    send_str(ft_atoi(argv[1]), argv[2]);
+
+    if (argc != 2)
         return (0);
-    kill(ft_atoi(argv[1]), SIGUSR1);
 
     while (1)
         pause();
--- server.c
+++ server.c
@@ -16,14 +16,31 @@
 
 static void sighandler(int sig)
 {
-    if (sig == SIGUSR1)
-        ft_printf("Segnale ricevuto");
-}
+    static int i = 0;
+    static unsigned char c = 0;
 
+    c = c | (sig == SIGUSR1);
+
+    if (++i == 8)
+    {
+        i = 0;
+        ft_printf("%c", c);
+        c = 0;
+    }
+    else
+        c <<= 1;
+}
 int main()
 {
+    struct sigaction sa;
+
+    sa.sa_handler = sighandler;
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
     ft_printf("%d\n", getpid());
-    signal(SIGUSR1, sighandler);
     while (1)
         pause();
+    return (0);
 }
